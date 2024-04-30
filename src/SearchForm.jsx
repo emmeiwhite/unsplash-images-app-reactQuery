ce546225cf0bfc6478453ddab41ad64933549222
Imran Rafiq Rather <emmeiwhite@gmail.com>
2024-04-30 12:30:56 +0530

    A non controlled input, nice for one input field search field

--- src/SearchForm.jsx
+++ src/SearchForm.jsx
@@ -1,3 +1,30 @@
 export default function SearchForm() {
-  return <div>SearchForm</div>;
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+
+    console.log(e.target.elements.search);
+  };
+  return (
+    <section>
+      <h1 className="title">Unsplash Images</h1>
+      <form
+        className="search-form"
+        onSubmit={handleFormSubmit}
+      >
+        <input
+          type="text"
+          className="form-input search-input"
+          name="search"
+          placeholder="cat"
+        />
+
+        <button
+          type="submit"
+          className="btn"
+        >
+          Search
+        </button>
+      </form>
+    </section>
+  );
 }
